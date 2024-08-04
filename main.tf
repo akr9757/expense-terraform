c3ae5cde50b0f952e0ef893d810670cb6f8e283d
akr9757 <anilkumareddy9757@gmail.com>
2024-08-04 19:51:15 +0530

    terraform code

--- main.tf
+++ main.tf
@@ -1,21 +1,10 @@
 resource "aws_instance" "instances" {
-  ami           = data.aws_ami.ami.id
-  instance_type = "t3.micro"
+  ami                        = data.aws_ami.ami.id
+  instance_type              = "t3.micro"
+  vpc_security_group_ids     = ["vpc-016b04b871ea2362c"]
 
   tags = {
     Name = "frontend"
   }
 }
 
-data "aws_ami" "ami" {
-  most_recent = true
-  owners = ["973714476881"]
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = "Z04275581JIKR4XEVM94K"
-  name    = "frontend-dev.akrdevops.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instances.private_ip]
-}
