4f102e7505f7623f063ff2d227ff604d9df0fe00
akr9757 <anilkumareddy9757@gmail.com>
2024-08-04 20:02:06 +0530

    terraform code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instances" {
   ami                        = data.aws_ami.ami.id
   instance_type              = "t3.micro"
-  vpc_security_group_ids     = ["vpc-016b04b871ea2362c"]
+  vpc_security_group_ids     = [aws_security_group.allow-all.id]
 
   tags = {
     Name = "frontend"
@@ -14,4 +14,14 @@ resource "aws_route53_record" "record" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.instances.private_ip]
+}
+
+resource "aws_security_group" "allow-all" {
+  name        = "allow-all"
+  description = "Allow-all"
+  vpc_id      = "vpc-016b04b871ea2362c"
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
