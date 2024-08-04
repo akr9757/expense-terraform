41ea52be13d1df4cdbc6862c337dee9c47cc0c76
akr9757 <anilkumareddy9757@gmail.com>
2024-08-04 21:31:23 +0530

    terraform code

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,2 +1,18 @@
 env="dev"
 
+variable "components" {
+  main = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    backend = {
+      name = "backend"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+  }
+}
--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
 resource "aws_instance" "instances" {
+  for_each = var.components
   ami                        = data.aws_ami.ami.id
-  instance_type              = "t3.micro"
+  instance_type              = each.value["instance_type"]
   vpc_security_group_ids     = ["sg-06d14744e7a12dcaf"]
 
   tags = {
-    Name = "frontend"
+    Name = each.value["name"]
   }
 }
 
 resource "aws_route53_record" "record" {
+  for_each = var.components
   zone_id = "Z04275581JIKR4XEVM94K"
-  name    = "frontend-dev.akrdevops.online"
+  name    = "${each.value["name"]}-${var.env}.akrdevops.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.instances.private_ip]
+  records = [aws_instance.instances[each.value["name"]].private_ip]
 }
 
