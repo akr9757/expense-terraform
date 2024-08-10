b7f0f7fbcdcdc77a0c214734d3d1d9498292cb0b
akr9757 <anilkumareddy9757@gmail.com>
2024-08-10 16:41:04 +0530

    terraform code

--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
 resource "aws_instance" "instances" {
-  for_each                   = var.components
-  ami                        = data.aws_ami.ami.id
-  instance_type              = lookup(each.value, "instance_type", null)
-  vpc_security_group_ids     = ["sg-06d14744e7a12dcaf"]
+  for_each = var.components
+  ami           = data.aws_ami.ami
+  instance_type = lookup(each.value, "instance_type", null)
+  vpc_security_group_ids = ["sg-06d14744e7a12dcaf"]
 
   tags = {
     Name = lookup(each.value, "name", null)
   }
 }
 
-resource "aws_route53_record" "record" {
+resource "aws_route53_record" "main" {
   for_each = var.components
   zone_id = "Z04275581JIKR4XEVM94K"
-  name    = "${lookup(each.value, "name", null)}-${var.env}.akrdevops.online"
+  name    = lookup(each.value, "name", null)
   type    = "A"
   ttl     = 30
-  records = [aws_instance.instances[lookup(each.value, "name", null)].private_ip]
+  records = [aws_instance.instances[each.key].private_ip]
 }
-
