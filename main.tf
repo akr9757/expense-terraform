bcc0b912f3116217006cf2f5ad374395bc78bba5
akr9757 <anilkumareddy9757@gmail.com>
2024-08-12 13:20:43 +0530

    vpc module

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ module "vpc" {
 
   cidr_block           = lookup(each.value, "cidr_block", null)
   env                  = var.env
-  subnets_cidr         = lookup(each.value, "subnets_cidr" null)
-  az         = lookup(each.value, "az" null)
+  subnets_cidr         = lookup(each.value, "subnets_cidr", null)
+  az         = lookup(each.value, "az", null)
 }
