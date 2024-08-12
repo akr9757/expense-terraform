776c73a73bdb1a676ea9a05315f0dca87c9070f0
akr9757 <anilkumareddy9757@gmail.com>
2024-08-12 14:50:12 +0530

    vpc module

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ module "vpc" {
 
   vpc_cidr_block                = lookup(each.value, "vpc_cidr_block", null)
   public_subnets_cidr           = lookup(each.value, "public_subnets_cidr", null)
-  web_subnet_cidr               = lookup(each.value, "web_subnets_cidr", null)
-  app_subnet_cidr               = lookup(each.value, "app_subnets_cidr", null)
-  db_subnet_cidr               = lookup(each.value, "db_subnets_cidr", null)
+  web_subnets_cidr               = lookup(each.value, "web_subnets_cidr", null)
+  app_subnets_cidr               = lookup(each.value, "app_subnets_cidr", null)
+  db_subnets_cidr               = lookup(each.value, "db_subnets_cidr", null)
   env                           = var.env
   az                            = lookup(each.value, "az", null)
 }
