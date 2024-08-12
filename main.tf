3d16429bf0e8db02a22650ed42430a11c897ea0e
akr9757 <anilkumareddy9757@gmail.com>
2024-08-12 12:36:55 +0530

    vpc module

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "./modules/vpc"
   for_each = var.vpc
 
-  cidr_block           = lookup(each.value, "cidr_block" null)
+  cidr_block           = lookup(each.value, "cidr_block", null)
   env                  = var.env
 #   public.subnet_cidr = lookup(each.value, "public.subnet_cidr" null)
 }
