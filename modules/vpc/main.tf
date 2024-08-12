27a9ccaff3549e02c4a28f4563889508d68c03f8
akr9757 <anilkumareddy9757@gmail.com>
2024-08-12 13:39:35 +0530

    vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -28,3 +28,15 @@ resource "aws_subnet" "main" {
     Name = "public-${count.index+1}"
   }
 }
+
+resource "aws_route" "main" {
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "default" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
