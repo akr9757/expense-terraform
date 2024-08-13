a5f2ef152180b8aa5280549eaf9154c040826baf
akr9757 <anilkumareddy9757@gmail.com>
2024-08-13 15:37:01 +0530

    vpc module

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "main" {
   ingress {
     from_port        = 3306
     to_port          = 3306
-    protocol         = "-1"
+    protocol         = "tcp"
     cidr_blocks      = var.sg_cidr_blocks
   }
 
