3013e878ab41f77e5bb9840c27bcafe32874e0b8
akr9757 <anilkumareddy9757@gmail.com>
2024-08-17 11:51:22 +0530

    vpc module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,61 +1,61 @@
-# resource "aws_lb" "main" {
-#   name               = local.name
-#   internal           = var.internal
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.main.id]
-#   subnets            = var.subnets
-#
-#
-#   tags = {
-#     Name = local.name
-#   }
-# }
-#
-# resource "aws_security_group" "main" {
-#   name        = "${ local.name }-sg"
-#   description = "${ local.name }-sg"
-#   vpc_id      = var.vpc_id
-#
-#
-#   ingress {
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = var.sg_cidr_blocks
-#     description      = "HTTP"
-#   }
-#
-#   ingress {
-#     from_port        = 443
-#     to_port          = 443
-#     protocol         = "tcp"
-#     cidr_blocks      = var.sg_cidr_blocks
-#     description      = "HTTPS"
-#   }
-#
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-#
-#   tags = {
-#     Name = "${ local.name }-sg"
-#   }
-# }
-#
-# resource "aws_lb_listener" "main" {
-#   load_balancer_arn = aws_lb.main.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = var.certificate_arn
-#
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = var.target_group_arn
-#   }
-# }
+resource "aws_lb" "main" {
+  name               = local.name
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
+
+
+  tags = {
+    Name = local.name
+  }
+}
+
+resource "aws_security_group" "main" {
+  name        = "${ local.name }-sg"
+  description = "${ local.name }-sg"
+  vpc_id      = var.vpc_id
+
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_cidr_blocks
+    description      = "HTTP"
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_cidr_blocks
+    description      = "HTTPS"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${ local.name }-sg"
+  }
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = var.target_group_arn
+  }
+}
 
 
