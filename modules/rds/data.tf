f603a7be937d971423d26aa985c1a1e6bd540806
akr9757 <anilkumareddy9757@gmail.com>
2024-08-28 18:00:46 +0530

    vpc module

--- modules/rds/data.tf
+++ modules/rds/data.tf
@@ -1,7 +1,7 @@
 data "aws_ssm_parameter" "username" {
-  name = "${var.env}.${var.project_name}.rds_username"
+  name = "${var.env}.${var.project_name}.rds.username"
 }
 
 data "aws_ssm_parameter" "password" {
-  name = "${var.env}.${var.project_name}.rds_password"
+  name = "${var.env}.${var.project_name}.rds.password"
 }
