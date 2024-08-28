dbcda07e7d44a215f73d8a6e80865f9cef2f37db
akr9757 <anilkumareddy9757@gmail.com>
2024-08-28 21:43:23 +0530

    vpc module

--- main.tf
+++ main.tf
@@ -86,9 +86,11 @@ module "backend" {
   sg_cidr_blocks    = lookup(lookup(var.vpc, "main", null), "app_subnets_cidr", null)
   vpc_id            = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   vpc_zone_identifier = lookup(lookup(module.vpc, "main", null), "app_subnets_ids", null)
-  parameters        = ["arn:aws:ssm:us-east-1:975050250738:parameter/${var.env}.${var.project_name}.rds.password"]
+  parameters        = ["arn:aws:ssm:us-east-1:975050250738:parameter/${var.env}.${var.project_name}.rds.*"]
   kms               = var.kms_key_id
 }
+
+
 
 module "frontend" {
   source = "./modules/app"
