7541f1d3ddbbfefd6a71eef9f7ebc3615a3c2b76
akr9757 <anilkumareddy9757@gmail.com>
2024-08-28 18:20:04 +0530

    vpc module

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -2,4 +2,4 @@
 
 yum install ansible python3.11-pip -y &>>/opt/userdata.log
 pip3.11 install botocore boto3 &>>/opt/userdata.log
-ansible-pull -i localhost -U https://github.com/akr9757/expense-ansible.git -e service_name=${service_name} -e env=${env} expense.yml &>>/opt/userdata.log
+ansible-pull -i localhost -U https://github.com/akr9757/expense-ansible.git -e service_name=${service_name} -e env=${env} expense.yml  &>>/opt/userdata.log
