5236379fc779523f52a21b8b3e7805be955b8916
akr9757 <anilkumareddy9757@gmail.com>
2024-08-28 10:28:53 +0530

    vpc module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -58,4 +58,20 @@ resource "aws_lb_listener" "main" {
   }
 }
 
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 
